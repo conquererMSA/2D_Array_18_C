6624c8be5c9bbef54a9c1b4e37842aa4d62fd39f
asus test <msa-99@outlook.com>
2024-05-06 15:36:26 +0600

    secodery diagonal unitMatrix

--- unitMatrix.c
+++ unitMatrix.c
@@ -1,38 +1,5 @@
 #include<stdio.h>
 //Primary Diagonal Unit Matrix:
-int main(){
-    int row,col;
-    scanf("%d %d",&row,&col);
-    int a[row][col];
-    for(int i=0; i<row; i++){
-        for(int j=0; j<col; j++){
-            scanf("%d",&a[i][j]);
-        }
-    }
-  int isUnitMatrix=1;
-    for(int i=0; i<row; i++){
-        for(int j=0; j<col; j++){
-           if(i==j){
-            if(a[i][j]!=1){
-              isUnitMatrix=0;
-            }
-            continue;
-           }
-
-          if(a[i][j]!=0){
-             isUnitMatrix=0;
-          }
-        }
-    }
-    if(isUnitMatrix){
-        printf("Unit Matrix");
-    }else{
-        printf("Not Unit Matrix");
-    }
-    return 0;
-}
-
-//Secondery diagonal matrix
 // int main(){
 //     int row,col;
 //     scanf("%d %d",&row,&col);
@@ -45,7 +12,7 @@ int main(){
 //   int isUnitMatrix=1;
 //     for(int i=0; i<row; i++){
 //         for(int j=0; j<col; j++){
-//            if(i+j==row-1){
+//            if(i==j){
 //             if(a[i][j]!=1){
 //               isUnitMatrix=0;
 //             }
@@ -64,3 +31,36 @@ int main(){
 //     }
 //     return 0;
 // }
+
+//Secondery diagonal matrix
+int main(){
+    int row,col;
+    scanf("%d %d",&row,&col);
+    int a[row][col];
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+  int isUnitMatrix=1;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+           if(i+j==row-1){
+            if(a[i][j]!=1){
+              isUnitMatrix=0;
+            }
+            continue;
+           }
+
+          if(a[i][j]!=0){
+             isUnitMatrix=0;
+          }
+        }
+    }
+    if(isUnitMatrix){
+        printf("Unit Matrix");
+    }else{
+        printf("Not Unit Matrix");
+    }
+    return 0;
+}
