c3cccaa300f61fa51044802e715080ca599f451b
asus test <msa-99@outlook.com>
2024-05-06 14:35:42 +0600

    zero/null matrix

--- zeroNullMatrix.c
+++ zeroNullMatrix.c
@@ -1,6 +1,27 @@
-//null matrix er sq/rect hote pare. row+col er value zero hobe.
+//null matrix er sq/rect hote pare. row+col er value zero hobe. null/zero matrix er sob gulu index er value 0 hobe.
 #include<stdio.h>
 int main(){
-    
+    int row,col;
+    scanf("%d %d",&row,&col);
+    int a[row][col];
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int isNullMatrix=1;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+           if(a[i][j]!=0){
+             isNullMatrix=0;
+             break;
+           }
+        }
+    }
+    if(isNullMatrix){
+        printf("Null Matrix");
+    }else{
+        printf("Not Null Matrix");
+    }
     return 0;
 }
