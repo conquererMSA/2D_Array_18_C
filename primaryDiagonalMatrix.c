4c52e7361fce099af1ec1515d31f48fa00d382b8
asus test <msa-99@outlook.com>
2024-05-06 14:18:34 +0600

    Primary diagonal

---
+++ primaryDiagonalMatrix.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main(){
+    int row,col;
+    scanf("%d %d",&row,&col);
+    int a[row][col];
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    // for(int i=0; i<row; i++){
+    //     for(int j=0; j<col; j++){
+    //         printf("%d ",a[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    int isDiagonal=1;
+    if(row!=col){
+        isDiagonal=0;
+    }
+   for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(a[i]==a[j]){
+                continue;
+            }
+            if(a[i][j]!=0){
+                isDiagonal=0;
+            }
+        }
+    }
+    if(isDiagonal){
+        printf("Primary Diagonal");
+    }else{
+        printf("Not Diagonal");
+    }
+
+    return 0;
+}
---
+++ zeroNullMatrix.c
@@ -0,0 +1,6 @@
+//null matrix er sq/rect hote pare. row+col er value zero hobe.
+#include<stdio.h>
+int main(){
+    
+    return 0;
+}
